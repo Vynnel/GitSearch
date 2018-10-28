b9648793c824a9b1ca313e1a08d57c8ccec566dc
Vynnel Lee <vynnellee@gmail.com>
2018-10-28 16:41:44 +0300

    added new files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GitProfileComponent } from './git-profile/git-profile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitProfileComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule
